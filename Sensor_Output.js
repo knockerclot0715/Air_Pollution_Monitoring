de1bb937852a9f324de531263174b4382aa98ef3
knockerclot0715 <zhoumarc@icloud.com>
2018-03-23 00:05:55 +0800

    Fixed the broken serial data converter

--- Sensor_Output.js
+++ Sensor_Output.js
@@ -1,32 +1,17 @@
 "use strict";
 
-console.log("Hi");
 
 const SerialPortModule = require("serialport");
-const Moment = require("moment");
-const MySQL = require("mysql");
+const MomentModule = require("moment");
+const MySQLModule = require("mysql");
 
-const SerialPort = new SerialPort("/dev/ttyUSB0", {baudrate: 9600});
-const DatabaseConnection = MySQL.createConnection({host: "10.0.0.11", user: "Station", password: "Marc0715", database: "Air_Pollution_Reading_Record"});
+const SerialPort = new SerialPortModule("/dev/ttyUSB0", {baudRate: 9600});
 
-while (true) {
-  SerialPort.on("Serial Data:", data);
-  var Data = data.toString('hex').match(/.{1,2}/g);
+
+SerialPort.on('data', function(Data) {
+  var Data = Data.toString('hex').match(/.{1,2}/g);
   var LowBitRate = parseInt(Data[2], 16);
   var HighBitRate = parseInt(Data[3], 16);
   var Air_Pollution_Reading = (HighBitRate * 256 + LowBitRate) / 10;
-  var CurrentTime = moment("00:00:00", "hh:mm:ss");
-  console.log("Current Time:", CurrentTime);
-  console.log("Current PM2.5 Reading", Air_Pollution_Reading);
-  DatabaseConnection.connect(function(Error) {
-    if (Error) {
-      console.log("Failed To Connect To The Database");
-      console.error(Error.stack);
-      return;
-    } else {
-      console.log("Connected To The Database");
-    }
-  });
-  DatabaseConnection.end();
-  console.log("-----------------");
-};
+  console.log("Air Pollution Reading:", Air_Pollution_Reading);
+});

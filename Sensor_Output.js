2f29a5c4e04d1434f68ad2d41da995b943ac0d00
knockerclot0715 <zhoumarc@icloud.com>
2018-03-23 00:58:43 +0800

    instaled npm module date-and-time. outputs current time. annotations.

--- Sensor_Output.js
+++ Sensor_Output.js
@@ -2,16 +2,25 @@
 
 
 const SerialPortModule = require("serialport");
-const MomentModule = require("moment");
 const MySQLModule = require("mysql");
+const DateTimeModule = require('date-and-time');
 
 const SerialPort = new SerialPortModule("/dev/ttyUSB0", {baudRate: 9600});
 
-
 SerialPort.on('data', function(Data) {
+
+  //Settings for serial data conversion and extraction
   var Data = Data.toString('hex').match(/.{1,2}/g);
   var LowBitRate = parseInt(Data[2], 16);
   var HighBitRate = parseInt(Data[3], 16);
   var Air_Pollution_Reading = (HighBitRate * 256 + LowBitRate) / 10;
+
+  //Settings for current time
+  let Time = new Date();
+  var Current_Time = DateTimeModule.format(Time, 'YYYY/MM/DD HH:mm:ss');
+
+  //output pm2.5 reading and the current time
+  console.log("Current Time:", Current_Time);
   console.log("Air Pollution Reading:", Air_Pollution_Reading);
+  console.log("--------------------");
 });

40e8f69d5a894c45279e2b99f9f7cf475496b1a8
knockerclot0715 <zhoumarc@icloud.com>
2018-03-23 23:20:58 +0800

    setting database settings and query

--- Sensor_Output.js
+++ Sensor_Output.js
@@ -2,10 +2,15 @@
 
 const SerialPortModule = require("serialport");
 const DateTimeModule = require('date-and-time');
+const MySQLModule = require('mysql');
 
 const SerialPort = new SerialPortModule("/dev/ttyUSB0", {baudRate: 9600});
+const Database_Connection = MySQLModule.createConnection({host: '10.0.0.254', user: 'MarcZhouToneu', password: 'Marc0715', database: 'Air_Pollution_Project'});
+
+Database_Connection.connect();
 
 SerialPort.on('data', function(Data) {
+  const Station_ID = 1;
 
   //Settings for serial data conversion and extraction
   var Data = Data.toString('hex').match(/.{1,2}/g);
@@ -20,5 +25,6 @@ SerialPort.on('data', function(Data) {
   //output pm2.5 reading and the current time
   console.log("Current Time:", Current_Time);
   console.log("Air Pollution Reading:", Air_Pollution_Reading);
+  Database_Connection.query('INSERT IGNORE INTO Air_Pollution_Reading_Record (Station_ID, Air_Pollution_Reading_Value) VALUES ('+Station_ID+', '+Air_Pollution_Reading+')');
   console.log("--------------------");
 });

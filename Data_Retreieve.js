dcafbabc0327d760fa464c1261751a220f5d9d76
knockerclot0715 <zhoumarc@icloud.com>
2018-03-25 13:38:46 +0800

    changed the time

--- Data_Retreieve.js
+++ Data_Retreieve.js
@@ -7,7 +7,7 @@ const Database_Connection = MySQLModule.createConnection({host: '10.0.0.11', use
 
 var Time = new Date();
 //var Current_Time = DateTimeModule.format(Time, 'YYYY-MM-DD HH:mm:ss');
-var Current_Time = "'2018-03-25 05:59:59'";
+var Current_Time = "'2018-03-25 05:00:59'";
 console.log("Current Time:", Current_Time);
 //var Previous_Time = DateTimeModule.addSeconds(Time, -10, 'YYYY-MM-DD HH:mm:ss');
 var Previous_Time = "'2018-03-25 05:00:00'";

e6d0b56abba96727b95d077e0d10a2668567b484
knockerclot0715 <zhoumarc@icloud.com>
2018-03-25 14:21:21 +0800

    Changed it back to the start

--- Data_Retreieve.js
+++ Data_Retreieve.js
@@ -15,18 +15,16 @@ console.log("Previous Time:", Previous_Time);
 
 Database_Connection.connect();
 
-Database_Connection.query('SELECT Air_Pollution_Reading_Value FROM Air_Pollution_Reading_Record WHERE Time_Of_Record > '+Previous_Time+' AND Time_Of_Record <= '+Current_Time+'', function(error, results, fields) {
+Database_Connection.query('SELECT Air_Pollution_Reading_Value FROM Air_Pollution_Reading_Record WHERE Time_Of_Record > '+Previous_Time+' AND Time_Of_Record <= '+Current_Time+'', function(error, results) {
   if (error) {
     console.log("An errror has occured", error.stack);
   } else {
     console.log("Raw Data From The Past 10 Seconds:");
     console.log(results);
     SaveResult(results);
-    console.log(fields);
   };
 });
 
 function SaveResult(Result) {
   QueryResults.push(Result);
-  console.log(Result);
 }

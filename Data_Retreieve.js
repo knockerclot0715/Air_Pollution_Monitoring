bcaefec94f5cb4bd6c38d0ba9eb29f4952b0692e
knockerclot0715 <zhoumarc@icloud.com>
2018-03-25 12:27:25 +0800

    Just output the Air_Pollution_Reading_Value

--- Data_Retreieve.js
+++ Data_Retreieve.js
@@ -12,7 +12,7 @@ var Previous_Time = "'2018-03-25 05:00:20'";
 console.log("Previous Time:", Previous_Time);
 
 Database_Connection.connect();
-Database_Connection.query('SELECT * FROM Air_Pollution_Reading_Record WHERE Time_Of_Record > '+Previous_Time+' AND Time_Of_Record <= '+Current_Time+'', function(error, results) {
+Database_Connection.query('SELECT Air_Pollution_Reading_Value FROM Air_Pollution_Reading_Record WHERE Time_Of_Record > '+Previous_Time+' AND Time_Of_Record <= '+Current_Time+'', function(error, results) {
   if (error) {
     console.log("An errror has occured", error.stack);
   } else {

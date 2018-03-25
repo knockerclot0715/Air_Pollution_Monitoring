8bc904c6378f07f8c92c50c536f77ff20ca8489e
knockerclot0715 <zhoumarc@icloud.com>
2018-03-25 11:41:54 +0800

    Changed line 13 so that it give debug info

--- Data_Retreieve.js
+++ Data_Retreieve.js
@@ -10,7 +10,7 @@ var Previous_Time = DateTimeModule.addSeconds(Time, -10);
 Database_Connection.connect();
 Database_Connection.query('SELECT * FROM Air_Pollution_Project.Air_Pollution_Reading_Record WHERE Time_Of_Record > '+Previous_Time+' AND Time_Of_Record <= '+Current_Time+'', function(error, results) {
   if (error) {
-    console.log("An errror has occured", error.thread);
+    console.log("An errror has occured", error.stack);
   } else {
     console.log("Raw Data From The Past 10 Seconds:");
     console.log(results);

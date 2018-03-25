a309ec7e57be2ca9a2aa3b1cf88bbd24e078b8f3
knockerclot0715 <zhoumarc@icloud.com>
2018-03-25 11:37:54 +0800

    Corrected line 8

--- Data_Retreieve.js
+++ Data_Retreieve.js
@@ -5,7 +5,7 @@ const Database_Connection = MySQLModule.createConnection({host: '10.0.0.11', use
 
 var Time = new Date();
 var Current_Time = DateTimeModule.format(Time, 'YYYY/MM/DD HH:mm:ss');
-var Previous_Time = date.addSeconds(Time, -10);
+var Previous_Time = DateTimeModule.addSeconds(Time, -10);
 
 Database_Connection.connect();
 Database_Connection.query('SELECT * FROM Air_Pollution_Project.Air_Pollution_Reading_Record WHERE Time_Of_Record > '+Previous_Time+' AND Time_Of_Record <= '+Current_Time+'', function(error, results) {

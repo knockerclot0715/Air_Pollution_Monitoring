d9fea32fc879b7423e2d4774f9d61262ca886277
knockerclot0715 <zhoumarc@icloud.com>
2018-03-25 13:31:44 +0800

    Trying to figure out how to get the length of the variable

--- Data_Retreieve.js
+++ Data_Retreieve.js
@@ -7,13 +7,14 @@ const Database_Connection = MySQLModule.createConnection({host: '10.0.0.11', use
 
 var Time = new Date();
 //var Current_Time = DateTimeModule.format(Time, 'YYYY-MM-DD HH:mm:ss');
-var Current_Time = "'2018-03-25 05:00:30'";
+var Current_Time = "'2018-03-25 05:59:59'";
 console.log("Current Time:", Current_Time);
 //var Previous_Time = DateTimeModule.addSeconds(Time, -10, 'YYYY-MM-DD HH:mm:ss');
-var Previous_Time = "'2018-03-25 05:00:20'";
+var Previous_Time = "'2018-03-25 05:00:00'";
 console.log("Previous Time:", Previous_Time);
 
 Database_Connection.connect();
+
 Database_Connection.query('SELECT Air_Pollution_Reading_Value FROM Air_Pollution_Reading_Record WHERE Time_Of_Record > '+Previous_Time+' AND Time_Of_Record <= '+Current_Time+'', function(error, results) {
   if (error) {
     console.log("An errror has occured", error.stack);
@@ -26,5 +27,7 @@ Database_Connection.query('SELECT Air_Pollution_Reading_Value FROM Air_Pollution
 
 function SaveResult(Result) {
   QueryResults.push(Result);
-  console.log(QueryResults)
+  var LengthOfArray = QueryResults.length;
+  console.log(LengthOfArray);
+  console.log(Result);
 }

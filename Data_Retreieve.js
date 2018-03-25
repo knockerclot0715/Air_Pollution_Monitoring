435bbedc1570bab09b217c57b26737607b81d7a6
knockerclot0715 <zhoumarc@icloud.com>
2018-03-25 15:01:13 +0800

    Added average statement to Air_Pollution_Reading_Value

--- Data_Retreieve.js
+++ Data_Retreieve.js
@@ -15,7 +15,7 @@ console.log("Previous Time:", Previous_Time);
 
 Database_Connection.connect();
 
-Database_Connection.query('SELECT Air_Pollution_Reading_Value FROM Air_Pollution_Reading_Record WHERE Time_Of_Record > '+Previous_Time+' AND Time_Of_Record <= '+Current_Time+'', function(error, results) {
+Database_Connection.query('SELECT AVG(Air_Pollution_Reading_Value) FROM Air_Pollution_Reading_Record WHERE Time_Of_Record > '+Previous_Time+' AND Time_Of_Record <= '+Current_Time+'', function(error, results) {
   if (error) {
     console.log("An errror has occured", error.stack);
   } else {

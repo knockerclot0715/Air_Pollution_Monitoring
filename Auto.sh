e3d850cab70551dfa1e88b12b9a42bf367132fee
knockerclot0715 <zhoumarc@icloud.com>
2018-03-26 20:44:55 +0800

    Added git reset, and git pull

--- Auto.sh
+++ Auto.sh
@@ -1,6 +1,10 @@
 echo "Executing Auto.sh File!"
 cd
 cd Project/Air_Pollution_Monitoring/
+echo "Reset local git copy"
+git reset --hard HEAD
+echo "git pull"
+git pull
 echo "Installing 'mysql' module"
 npm install mysql
 echo "Installing 'serialport' module"

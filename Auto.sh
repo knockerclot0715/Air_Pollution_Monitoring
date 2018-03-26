76f0cb962a98a363f94638420f04d0ce1ca3e2a9
knockerclot0715 <zhoumarc@icloud.com>
2018-03-26 20:36:43 +0800

    Reformat the shell script

--- Auto.sh
+++ Auto.sh
@@ -1,7 +1,6 @@
-echo "You're about to execute the automated stuff"
-cd
-ls
-cd Project
-ls
-cd Air_Pollution_Monitoring
-ls
+echo "Executing Auto.sh File!"
+cd Project/Air_Pollution_Monitoring/
+npm install mysql
+npm install serialport
+npm install date-and-time
+node Sensor_Output.js

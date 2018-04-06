1c6e7f934c49d2febfa6f13a3ffb9398da239335
knockerclot0715 <zhoumarc@icloud.com>
2018-04-06 22:29:19 +0800

    Corrected mysql syntax

--- Date_Test.js
+++
@@ -1,6 +0,0 @@
-var MomentModule = require('moment');
-
-var Current_Time = MomentModule().format('YYYY-MM-DD H:m:s');
-var Previous_Time = MomentModule().subtract(30, 'seconds').format('YYYY-MM-DD H:m:s');
-console.log(Current_Time);
-console.log(Previous_Time);
